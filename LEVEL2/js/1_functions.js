d6d450a735a5f634d41253ffb44cfcf60151106b
Vedant <vedantdumbre0706@gmail.com>
2025-01-26 20:31:16 +0530

    functions in js

---
+++ LEVEL2/js/1_functions.js
@@ -0,0 +1,15 @@
+function repeat(name) {
+    return("hey !"+ name);
+
+}
+
+ans= repeat('john');
+console.log("the result is :",ans)
+
+// ARROw function in javascript
+
+var func1 =(a) => {
+    console.log("the value of a is :",a)
+}
+
+func1(10)
