aa2d8c9959a91611cc686bd898de01e12a7c0e5a
Vedant <vedantdumbre0706@gmail.com>
2025-01-30 21:48:50 +0530

    ex 4 done

--- LEVEL2/excercise/ex_4.js
+++ LEVEL2/excercise/ex_4.js
@@ -1,1 +1,21 @@
-console.log("dfhfjn")
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+    const box = document.querySelector(".box");
+
+    // Creating and appending elements
+    box.querySelector(".title").textContent = title;
+    box.querySelector(".cName").textContent = cName;
+    box.querySelector(".views").textContent = views;
+    box.querySelector(".monthsOld").textContent = monthsOld;
+    box.querySelector(".duration").textContent = duration;
+    box.querySelector(".thumbnail").style.backgroundImage = `url(${thumbnail})`;
+}
+
+// Calling the function with dummy data
+createCard(
+    "Learn JavaScript in 10 Minutes",
+    "Code Academy",
+    "1.2M views",
+    "3 months ago",
+    "10:15",
+    "https://i.ytimg.com/vi/BGeDBfCIqas/hqdefault.jpg"
+);
